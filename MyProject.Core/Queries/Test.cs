ff0400bf37384d313a43a8f955533d23ccd6ce77
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-02-05 07:03:18 +0900

    잘못된 이름 명명 수정

--- MyProject.Core/Queries/Test.cs
+++ MyProject.Core/Queries/Test.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MyProject.Core.Queries
 {
     [For(UserType.Administrator)]
-    public record TestQuery : ICoreRequest<TestErrorResult, TestResultView>
+    public record TestQuery : ICoreRequest<TestErrorReason, TestResultView>
     {
         public UserIdentity Identity { get; set; }
 
@@ -16,29 +16,29 @@ namespace MyProject.Core.Queries
         public int Argument2 { get; set; }
     }
 
-    public enum TestErrorResult
+    public enum TestErrorReason
     {
         UnknownError = 0,
         ValueIsNegative = 1,
         OutOfBound = 2,
     }
 
-    public class TestQueryHandler : ICoreRequestHandler<TestQuery, TestErrorResult, TestResultView>
+    public class TestQueryHandler : ICoreRequestHandler<TestQuery, TestErrorReason, TestResultView>
     {
-        public async Task<Result<TestErrorResult, TestResultView>> Handle(TestQuery request, CancellationToken cancellationToken)
+        public async Task<Result<TestErrorReason, TestResultView>> Handle(TestQuery request, CancellationToken cancellationToken)
         {
             int result = request.Argument1 + request.Argument2;
 
             if (result < 0)
-                return Result<TestErrorResult, TestResultView>.MakeFailure(TestErrorResult.ValueIsNegative);
+                return Result<TestErrorReason, TestResultView>.MakeFailure(TestErrorReason.ValueIsNegative);
 
             if (result > 20)
-                return Result<TestErrorResult, TestResultView>.MakeFailure(TestErrorResult.OutOfBound);
+                return Result<TestErrorReason, TestResultView>.MakeFailure(TestErrorReason.OutOfBound);
 
             if (result == 13)
                 throw new ArgumentException();
 
-            return Result<TestErrorResult, TestResultView>.MakeSuccess(new TestResultView
+            return Result<TestErrorReason, TestResultView>.MakeSuccess(new TestResultView
             {
                 Result = result
             });
