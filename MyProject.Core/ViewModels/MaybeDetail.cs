76f65ac0ad89d601cb10bd1053f11ed2824bc2fb
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-02-28 10:55:46 +0900

    MaybeDetail 레코드 추가

---
+++ MyProject.Core/ViewModels/MaybeDetail.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+namespace MyProject.Core.ViewModels
+{
+    /// <summary>
+    /// Detail 뷰모델을 따로 가질 때, 부모 또는 자식 클래스가 올 수 있음을 나타내는 타입
+    /// </summary>
+    /// <typeparam name="TBase">부모 클래스</typeparam>
+    /// <typeparam name="TDerived"><c>TBase</c>를 상속한 Detail 뷰모델</typeparam>
+    public record MaybeDetail<TBase, TDerived>(bool Detail, TBase Data)
+        where TDerived : TBase
+    {
+        public static implicit operator MaybeDetail<TBase, TDerived>(TBase @base)
+            => new MaybeDetail<TBase, TDerived>(@base is TDerived, @base);
+
+        public static implicit operator MaybeDetail<TBase, TDerived>(TDerived derived)
+            => new MaybeDetail<TBase, TDerived>(true, derived);
+    }
+}
